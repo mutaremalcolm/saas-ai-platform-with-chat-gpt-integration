d665498a29eac22835fd8a2cf4649088cddcface
“malcolmmutare” <mutarem@gmail.com>
2024-09-13 13:44:29 +0200

    Initialize Next.js project with Tailwind CSS setup
    - Created new Next.js project
    - Installed necessary dependencies
    - Configured Tailwind CSS for styling

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+const DashboardPage = () => {
+  return (
+    <div>Dashboard Page (Protected)</div>
+  );
+}
+
+export default DashboardPage;
+
