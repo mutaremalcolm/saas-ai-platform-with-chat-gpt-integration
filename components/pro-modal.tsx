ca1987a456ba59ce1e8469bc1406b93061f93a42
“malcolmmutare” <mutarem@gmail.com>
2024-10-01 16:41:25 +0200

    feat: added dialog component from shadcn, to prompt users to upgrade to premium

--- components/pro-modal.tsx
+++ components/pro-modal.tsx
@@ -1,9 +1,27 @@
 "use client";
 
+import { useProModal } from "@/hooks/use-pro-modal";
+import { 
+    Dialog, 
+    DialogContent, 
+    DialogTitle,
+    DialogHeader
+} from "./ui/dialog";
+
 export const ProModal = () => {
+    const proModal = useProModal();
+
     return (
         <div>
-            Pro Modal
+            <Dialog open={proModal.isOpen} onOpenChange={proModal.onClose}>
+                <DialogContent>
+                    <DialogHeader>
+                        <DialogTitle className="flex justify-center items-center flex-col gap-y-4 pb-2">
+                            Upgrade to Genius
+                        </DialogTitle>
+                    </DialogHeader>
+                </DialogContent>
+            </Dialog>
         </div>
     )
 }
