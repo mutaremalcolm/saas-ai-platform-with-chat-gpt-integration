aa3ca5d7d94b4e8db351bca50c4f082c59cfa1b8
“malcolmmutare” <mutarem@gmail.com>
2024-10-03 14:34:29 +0200

    feat: completed stripe integration - added appropriate routes for the subscription process - added conditional rendering to featured based on subscription status of the user

--- components/free-counter.tsx
+++ components/free-counter.tsx
@@ -11,10 +11,12 @@ import { useProModal } from "@/hooks/use-pro-modal";
 
 interface FreeCounterProps {
     apiLimitCount: number;
+    isPro: boolean;
 }
 
 export const FreeCounter = ({
-    apiLimitCount = 0
+    apiLimitCount = 0,
+    isPro = false,
 }: FreeCounterProps) => {
     const proModal = useProModal();
     const [mounted, setMounted] = useState(false);
@@ -27,6 +29,9 @@ export const FreeCounter = ({
         return null;
     }
 
+    if (isPro) {
+        return null;
+    }
 
     return (
         <div className="px-3">
