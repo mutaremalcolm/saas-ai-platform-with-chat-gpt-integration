c20ec365b86a0abaca6d96d8071c41d1b2eeaf01
“malcolmmutare” <mutarem@gmail.com>
2024-10-03 18:30:21 +0200

    feat: added react toast for improved error handling. Errors are displayed through toasts instead of console logs for a better user experience

--- app/(dashboard)/(routes)/music/page.tsx
+++ app/(dashboard)/(routes)/music/page.tsx
@@ -15,6 +15,7 @@ import { useState } from "react";
 import { Empty } from "@/components/empty";
 import { Loader } from "@/components/loader";
 import { useProModal } from "@/hooks/use-pro-modal";
+import toast from "react-hot-toast";
 
 
 
@@ -43,7 +44,9 @@ const MusicPage = () => {
     } catch (error: any) {
       if (error?.response?.status === 403) {
         proModal.onOpen();
-        }
+        }else {
+          toast.error("Something went wrong");
+        } 
     } finally {
       router.refresh();
     }
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import localFont from "next/font/local";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { ModalProvider } from "@/components/modal-provider";
+import { ToasterProvider } from "@/components/toaster-provider";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -32,6 +33,7 @@ export default function RootLayout({
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
           <ModalProvider />
+          <ToasterProvider />
           {children}
         </body>
       </html>
